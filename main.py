e496597495def1187ff65faffde7299285864cec
Nikita <nikitamarshchonok@gmail.com>
2023-10-15 17:48:06 +0300

    thank u

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import calculator
 
-
+#thakyuykk
 if __name__ == '__main__':
         calc = calculator.Calculator()
         calc.add_car(calculator.Car("Toyota Rav4", price=30000, fuel_economy=7, service_cost=1200, insurance_cost=2500))
